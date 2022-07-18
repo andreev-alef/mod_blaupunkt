ae957bd6079d9d3af1c0068b260c16f6abbda93c
Андреев Александр Филиппович <a.andreev@corp.npcmr.ru>
2022-07-18 16:10:57 +0300

    testing

--- blaupunkt/lib.php
+++ blaupunkt/lib.php
@@ -1,8 +1,16 @@
 <?php
 
-// function blaupunkt_add_instance($blaupunkt);
-// function blaupunkt_update_instance($blaupunkt);
-// function blaupunkt_delete_instance($id);
+function blaupunkt_add_instance($blaupunkt) {
+    
+}
+
+function blaupunkt_update_instance($blaupunkt) {
+    
+}
+
+function blaupunkt_delete_instance($id) {
+    
+}
 
 function tool_blaupunkt_extend_navigation_course($navigation, $course, $coursecontext) {
     $url = new moodle_url('/admin/tool/blaupunkt/index.php');
