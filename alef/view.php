84c70c1130b43bcd6208427b6af0349db34a97e5
Андреев Александр Филиппович <a.andreev@corp.npcmr.ru>
2022-07-21 15:17:50 +0300

    Эксперименты с просмотром модуля

--- alef/view.php
+++ alef/view.php
@@ -1,20 +1,5 @@
 <?php
 
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * alef module version information
  *
@@ -23,28 +8,29 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 require('../../config.php');
-require_once($CFG->dirroot . '/mod/alef/lib.php');
+//require_once($CFG->dirroot . '/mod/alef/lib.php');
 //require_once($CFG->dirroot . '/mod/alef/locallib.php');
-require_once($CFG->libdir . '/completionlib.php');
+//require_once($CFG->libdir . '/completionlib.php');
 
 $id = optional_param('id', 0, PARAM_INT); // Course Module ID
 
 
 $p = optional_param('p', 0, PARAM_INT);  // alef instance ID
 
-//$inpopup = optional_param('inpopup', 0, PARAM_BOOL);
-//
-//if ($p) {
-//    if (!$alef = $DB->get_record('alef', array('id' => $p))) {
-//        print_error('invalidaccessparameter');
-//    }
-//    $cm = get_coursemodule_from_instance('alef', $alef->id, $alef->course, false, MUST_EXIST);
-//} else {
-//    if (!$cm = get_coursemodule_from_id('alef', $id)) {
-//        print_error('invalidcoursemodule');
-//    }
-//    $alef = $DB->get_record('alef', array('id' => $cm->instance), '*', MUST_EXIST);
-//}
+$inpopup = optional_param('inpopup', 0, PARAM_BOOL);
+
+if ($p) {
+    if (!$alef = $DB->get_record('alef', array('id' => $p))) {
+        print_error('invalidaccessparameter');
+    }
+    $cm = get_coursemodule_from_instance('alef', $alef->id, $alef->course, false, MUST_EXIST);
+} else {
+    echo '<hr /><hr />';
+    if (!$cm = get_coursemodule_from_id('alef', $id)) {
+        print_error('invalidcoursemodule');
+    }
+    $alef = $DB->get_record('alef', array('id' => $cm->instance), '*', MUST_EXIST);
+}
 
 //$course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
 //
@@ -105,3 +91,5 @@ $p = optional_param('p', 0, PARAM_INT);  // alef instance ID
 <?= $id ?>
 <hr />
 <?= $p ?>
+<hr />
+<?= var_dump($alef) ?>
