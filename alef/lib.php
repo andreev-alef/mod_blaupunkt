5ce15193136dd175a13054eb965112f801963d20
Андреев Александр Филиппович <a.andreev@corp.npcmr.ru>
2022-07-21 12:47:22 +0300

    Добавляем свой модуль в панель модулей через файл lib.php

--- alef/lib.php
+++ alef/lib.php
@@ -1,5 +1,6 @@
 <?php
 // Добавляем свой модуль в панель модулей
+
 function tool_alef_extend_navigation_course($navigation, $course, $coursecontext) {
     $url = new moodle_url('/admin/tool/alef/index.php');
     $alefnode = navigation_node::create('Development course', $url, navigation_node::TYPE_CUSTOM, 'Dev course', 'alef');
