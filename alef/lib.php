2c13d431bf30197398fff25cc506d14080308c25
Андреев Александр Филиппович <a.andreev@corp.npcmr.ru>
2022-07-21 15:11:10 +0300

    Эксперименты с просмотром модуля

--- alef/lib.php
+++ alef/lib.php
@@ -1,8 +1,208 @@
 <?php
+defined('MOODLE_INTERNAL') || die;
+
 // Добавляем свой модуль в панель модулей
 
 function tool_alef_extend_navigation_course($navigation, $course, $coursecontext) {
     $url = new moodle_url('/admin/tool/alef/index.php');
     $alefnode = navigation_node::create('Development course', $url, navigation_node::TYPE_CUSTOM, 'Dev course', 'alef');
     $navigation->add_node($alefnode);
+}
+
+
+//Добавление элемента в курс
+
+
+/**
+ * List of features supported in alef module
+ * @param string $feature FEATURE_xx constant for requested feature
+ * @return mixed True if module supports feature, false if not, null if doesn't know
+ */
+//function alef_supports($feature) {
+//    switch($feature) {
+//        case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
+//        case FEATURE_GROUPS:                  return false;
+//        case FEATURE_GROUPINGS:               return false;
+//        case FEATURE_MOD_INTRO:               return true;
+//        case FEATURE_COMPLETION_TRACKS_VIEWS: return true;
+//        case FEATURE_GRADE_HAS_GRADE:         return false;
+//        case FEATURE_GRADE_OUTCOMES:          return false;
+//        case FEATURE_BACKUP_MOODLE2:          return true;
+//        case FEATURE_SHOW_DESCRIPTION:        return true;
+//
+//        default: return null;
+//    }
+//}
+
+/**
+ * This function is used by the reset_course_userdata function in moodlelib.
+ * @param $data the data submitted from the reset course.
+ * @return array status array
+ */
+//function alef_reset_userdata($data) {
+//
+//    // Any changes to the list of dates that needs to be rolled should be same during course restore and course reset.
+//    // See MDL-9367.
+//
+//    return array();
+//}
+
+/**
+ * List the actions that correspond to a view of this module.
+ * This is used by the participation report.
+ *
+ * Note: This is not used by new logging system. Event with
+ *       crud = 'r' and edulevel = LEVEL_PARTICIPATING will
+ *       be considered as view action.
+ *
+ * @return array
+ */
+function alef_get_view_actions() {
+    return array('view','view all');
+}
+
+/**
+ * List the actions that correspond to a post of this module.
+ * This is used by the participation report.
+ *
+ * Note: This is not used by new logging system. Event with
+ *       crud = ('c' || 'u' || 'd') and edulevel = LEVEL_PARTICIPATING
+ *       will be considered as post action.
+ *
+ * @return array
+ */
+function alef_get_post_actions() {
+    return array('update', 'add');
+}
+
+/**
+ * Add alef instance.
+ * @param stdClass $data
+ * @param mod_alef_mod_form $mform
+ * @return int new alef instance id
+ */
+function alef_add_instance($data, $mform = null) {
+    global $CFG, $DB;
+    require_once("$CFG->libdir/resourcelib.php");
+
+    $cmid = $data->coursemodule;
+
+    $data->timemodified = time();
+    $displayoptions = array();
+    if ($data->display == RESOURCELIB_DISPLAY_POPUP) {
+        $displayoptions['popupwidth']  = $data->popupwidth;
+        $displayoptions['popupheight'] = $data->popupheight;
+    }
+    $displayoptions['printheading'] = $data->printheading;
+    $displayoptions['printintro']   = $data->printintro;
+    $displayoptions['printlastmodified'] = $data->printlastmodified;
+    $data->displayoptions = serialize($displayoptions);
+
+    if ($mform) {
+        $data->content       = $data->alef['text'];
+        $data->contentformat = $data->alef['format'];
+    }
+
+    $data->id = $DB->insert_record('alef', $data);
+
+    // we need to use context now, so we need to make sure all needed info is already in db
+    $DB->set_field('course_modules', 'instance', $data->id, array('id'=>$cmid));
+    $context = context_module::instance($cmid);
+
+    if ($mform and !empty($data->alef['itemid'])) {
+        $draftitemid = $data->alef['itemid'];
+        $data->content = file_save_draft_area_files($draftitemid, $context->id, 'mod_alef', 'content', 0, alef_get_editor_options($context), $data->content);
+        $DB->update_record('alef', $data);
+    }
+
+    $completiontimeexpected = !empty($data->completionexpected) ? $data->completionexpected : null;
+    \core_completion\api::update_completion_date_event($cmid, 'alef', $data->id, $completiontimeexpected);
+
+    return $data->id;
+}
+
+/**
+ * Update alef instance.
+ * @param object $data
+ * @param object $mform
+ * @return bool true
+ */
+function alef_update_instance($data, $mform) {
+    global $CFG, $DB;
+    require_once("$CFG->libdir/resourcelib.php");
+
+    $cmid        = $data->coursemodule;
+    $draftitemid = $data->alef['itemid'];
+
+    $data->timemodified = time();
+    $data->id           = $data->instance;
+    $data->revision++;
+
+    $displayoptions = array();
+    if ($data->display == RESOURCELIB_DISPLAY_POPUP) {
+        $displayoptions['popupwidth']  = $data->popupwidth;
+        $displayoptions['popupheight'] = $data->popupheight;
+    }
+    $displayoptions['printheading'] = $data->printheading;
+    $displayoptions['printintro']   = $data->printintro;
+    $displayoptions['printlastmodified'] = $data->printlastmodified;
+    $data->displayoptions = serialize($displayoptions);
+
+    $data->content       = $data->alef['text'];
+    $data->contentformat = $data->alef['format'];
+
+    $DB->update_record('alef', $data);
+
+    $context = context_module::instance($cmid);
+    if ($draftitemid) {
+        $data->content = file_save_draft_area_files($draftitemid, $context->id, 'mod_alef', 'content', 0, alef_get_editor_options($context), $data->content);
+        $DB->update_record('alef', $data);
+    }
+
+    $completiontimeexpected = !empty($data->completionexpected) ? $data->completionexpected : null;
+    \core_completion\api::update_completion_date_event($cmid, 'alef', $data->id, $completiontimeexpected);
+
+    return true;
+}
+
+/**
+ * Delete alef instance.
+ * @param int $id
+ * @return bool true
+ */
+function alef_delete_instance($id) {
+    global $DB;
+
+    if (!$alef = $DB->get_record('alef', array('id'=>$id))) {
+        return false;
+    }
+
+    $cm = get_coursemodule_from_instance('alef', $id);
+    \core_completion\api::update_completion_date_event($cm->id, 'alef', $id, null);
+
+    // note: all context files are deleted automatically
+
+    $DB->delete_records('alef', array('id'=>$alef->id));
+
+    return true;
+}
+
+//Просмотреть содержимое модуля
+function alef_view($alef, $course, $cm, $context) {
+
+    // Trigger course_module_viewed event.
+    $params = array(
+        'context' => $context,
+        'objectid' => $alef->id
+    );
+
+    $event = \mod_alef\event\course_module_viewed::create($params);
+    $event->add_record_snapshot('course_modules', $cm);
+    $event->add_record_snapshot('course', $course);
+    $event->add_record_snapshot('alef', $alef);
+    $event->trigger();
+
+    // Completion.
+    $completion = new completion_info($course);
+    $completion->set_module_viewed($cm);
 }
--- alef/view.php
+++ alef/view.php
@@ -16,87 +16,92 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Page module version information
+ * alef module version information
  *
- * @package mod_alfand
- * @copyright  2009 Petr Skoda (http://skodak.org)
+ * @package mod_alef
+ * @copyright  2022 Alef
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 require('../../config.php');
-require_once($CFG->dirroot.'/mod/alfand/lib.php');
-require_once($CFG->dirroot.'/mod/alfand/locallib.php');
-require_once($CFG->libdir.'/completionlib.php');
+require_once($CFG->dirroot . '/mod/alef/lib.php');
+//require_once($CFG->dirroot . '/mod/alef/locallib.php');
+require_once($CFG->libdir . '/completionlib.php');
 
-$id      = optional_param('id', 0, PARAM_INT); // Course Module ID
-$p       = optional_param('p', 0, PARAM_INT);  // Page instance ID
-$inpopup = optional_param('inpopup', 0, PARAM_BOOL);
+$id = optional_param('id', 0, PARAM_INT); // Course Module ID
 
-if ($p) {
-    if (!$page = $DB->get_record('alfand', array('id'=>$p))) {
-        print_error('invalidaccessparameter');
-    }
-    $cm = get_coursemodule_from_instance('alfand', $page->id, $page->course, false, MUST_EXIST);
 
-} else {
-    if (!$cm = get_coursemodule_from_id('alfand', $id)) {
-        print_error('invalidcoursemodule');
-    }
-    $page = $DB->get_record('alfand', array('id'=>$cm->instance), '*', MUST_EXIST);
-}
+$p = optional_param('p', 0, PARAM_INT);  // alef instance ID
 
-$course = $DB->get_record('course', array('id'=>$cm->course), '*', MUST_EXIST);
+//$inpopup = optional_param('inpopup', 0, PARAM_BOOL);
+//
+//if ($p) {
+//    if (!$alef = $DB->get_record('alef', array('id' => $p))) {
+//        print_error('invalidaccessparameter');
+//    }
+//    $cm = get_coursemodule_from_instance('alef', $alef->id, $alef->course, false, MUST_EXIST);
+//} else {
+//    if (!$cm = get_coursemodule_from_id('alef', $id)) {
+//        print_error('invalidcoursemodule');
+//    }
+//    $alef = $DB->get_record('alef', array('id' => $cm->instance), '*', MUST_EXIST);
+//}
 
-require_course_login($course, true, $cm);
-$context = context_module::instance($cm->id);
-require_capability('mod/alfand:view', $context);
+//$course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+//
+//require_course_login($course, true, $cm);
+//$context = context_module::instance($cm->id);
+//require_capability('mod/alef:view', $context);
+//
+//// Completion and trigger events.
+//alef_view($alef, $course, $cm, $context);
+//
+//$alef->set_url('/mod/alef/view.php', array('id' => $cm->id));
+//
+//$options = empty($alef->displayoptions) ? array() : unserialize($alef->displayoptions);
+//
+//if ($inpopup and $alef->display == RESOURCELIB_DISPLAY_POPUP) {
+//    $alef->set_aleflayout('popup');
+//    $alef->set_title($course->shortname . ': ' . $alef->name);
+//    $alef->set_heading($course->fullname);
+//} else {
+//    $alef->set_title($course->shortname . ': ' . $alef->name);
+//    $alef->set_heading($course->fullname);
+//    $alef->set_activity_record($alef);
+//}
+//echo $OUTPUT->header();
+//if (!isset($options['printheading']) || !empty($options['printheading'])) {
+//    echo $OUTPUT->heading(format_string($alef->name), 2);
+//}
+//
+//// Display any activity information (eg completion requirements / dates).
+//$cminfo = cm_info::create($cm);
+//$completiondetails = \core_completion\cm_completion_details::get_instance($cminfo, $USER->id);
+//$activitydates = \core\activity_dates::get_dates_for_module($cminfo, $USER->id);
+//echo $OUTPUT->activity_information($cminfo, $completiondetails, $activitydates);
+//
+//if (!empty($options['printintro'])) {
+//    if (trim(strip_tags($alef->intro))) {
+//        echo $OUTPUT->box_start('mod_introbox', 'alefintro');
+//        echo format_module_intro('alef', $alef, $cm->id);
+//        echo $OUTPUT->box_end();
+//    }
+//}
+//
+//$content = file_rewrite_pluginfile_urls($alef->content, 'pluginfile.php', $context->id, 'mod_alef', 'content', $alef->revision);
+//$formatoptions = new stdClass;
+//$formatoptions->noclean = true;
+//$formatoptions->overflowdiv = true;
+//$formatoptions->context = $context;
+//$content = format_text($content, $alef->contentformat, $formatoptions);
+//echo $OUTPUT->box($content, "generalbox center clearfix");
+//
+//if (!isset($options['printlastmodified']) || !empty($options['printlastmodified'])) {
+//    $strlastmodified = get_string("lastmodified");
+//    echo html_writer::div("$strlastmodified: " . userdate($alef->timemodified), 'modified');
+//}
 
-// Completion and trigger events.
-page_view($page, $course, $cm, $context);
-
-$PAGE->set_url('/mod/alfand/view.php', array('id' => $cm->id));
-
-$options = empty($page->displayoptions) ? array() : unserialize($page->displayoptions);
-
-if ($inpopup and $page->display == RESOURCELIB_DISPLAY_POPUP) {
-    $PAGE->set_pagelayout('popup');
-    $PAGE->set_title($course->shortname.': '.$page->name);
-    $PAGE->set_heading($course->fullname);
-} else {
-    $PAGE->set_title($course->shortname.': '.$page->name);
-    $PAGE->set_heading($course->fullname);
-    $PAGE->set_activity_record($page);
-}
-echo $OUTPUT->header();
-if (!isset($options['printheading']) || !empty($options['printheading'])) {
-    echo $OUTPUT->heading(format_string($page->name), 2);
-}
-
-// Display any activity information (eg completion requirements / dates).
-$cminfo = cm_info::create($cm);
-$completiondetails = \core_completion\cm_completion_details::get_instance($cminfo, $USER->id);
-$activitydates = \core\activity_dates::get_dates_for_module($cminfo, $USER->id);
-echo $OUTPUT->activity_information($cminfo, $completiondetails, $activitydates);
-
-if (!empty($options['printintro'])) {
-    if (trim(strip_tags($page->intro))) {
-        echo $OUTPUT->box_start('mod_introbox', 'pageintro');
-        echo format_module_intro('alfand', $page, $cm->id);
-        echo $OUTPUT->box_end();
-    }
-}
-
-$content = file_rewrite_pluginfile_urls($page->content, 'pluginfile.php', $context->id, 'mod_alfand', 'content', $page->revision);
-$formatoptions = new stdClass;
-$formatoptions->noclean = true;
-$formatoptions->overflowdiv = true;
-$formatoptions->context = $context;
-$content = format_text($content, $page->contentformat, $formatoptions);
-echo $OUTPUT->box($content, "generalbox center clearfix");
-
-if (!isset($options['printlastmodified']) || !empty($options['printlastmodified'])) {
-    $strlastmodified = get_string("lastmodified");
-    echo html_writer::div("$strlastmodified: " . userdate($page->timemodified), 'modified');
-}
-
-echo $OUTPUT->footer();
+//echo $OUTPUT->footer();
+?>
+<?= $id ?>
+<hr />
+<?= $p ?>
